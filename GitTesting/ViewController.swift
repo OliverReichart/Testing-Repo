166e7899e48503b5efe05622dbe3537fe4dca733
Oliver Reichart <oliverreichart@dedaimac5kjre.fritz.box>
2018-05-08 18:23:23 +0200

    new revolutionary changes

--- GitTesting/ViewController.swift
+++ GitTesting/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var test = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
