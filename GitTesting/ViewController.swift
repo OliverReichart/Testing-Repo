c6c1287d0babd0e429ff04f4bc23c2f7e22daa1d
Oliver Reichart <oliverreichart@dedaimac5kjre.fritz.box>
2018-05-08 18:14:09 +0200

    Initial Commit

---
+++ GitTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTesting
+//
+//  Created by Oliver Reichart on 08.05.18.
+//  Copyright © 2018 Oliver Reichart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
